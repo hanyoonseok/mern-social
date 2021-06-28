6d4a6f72e2d7ca3a19457d5cd601cb2b50947a4b
한윤석 <hys6078@naver.com>
2021-06-28 13:17:40 +0900

    implement logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useContext } from 'react';
+import {Link,useHistory} from 'react-router-dom'
+import { UserContext } from '../App';
 
 //a 태그 대신 Link 쓴다고 생각하면 편함.
 //a태그는 누를때마다 새로고침됨 Link는 그냥 바로 바뀜
 //Link로 바뀌면 href 대신 to 사용
 const NavBar=()=>{
+    const {state,dispatch} = useContext(UserContext)
+    const history = useHistory()
+    const renderList = ()=>{
+        if(state){
+            return[
+                <li><Link to="/profile">Profile</Link></li>,
+                <li><Link to="/create">Create Post</Link></li>,
+                <li>
+                    <button className="btn #c62828 red darken-3" 
+                    onClick={()=>{
+                    localStorage.clear()
+                    dispatch({type:"CLEAR"})
+                    history.push('/signin')}}>Logout</button>    
+                </li>,
+            ]
+        }else{
+            return[
+                <li><Link to="/signin">Signin</Link></li>,
+                <li><Link to="/signup">Signup</Link></li>
+            ]
+        }
+    }
     return(
         <nav>
             <div className="nav-wrapper white" >
-                <Link to="/" className="brand-logo left">Hanstagram</Link>
+                <Link to={state?"/":"/signin"} className="brand-logo left">Hanstagram</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><Link to="/signin">Signin</Link></li>
-                    <li><Link to="/signup">SignUp</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/create">Create Post</Link></li>
+                    {renderList()}
                 </ul>
             </div> 
         </nav>
