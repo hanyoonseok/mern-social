6814f2ef710d475ca11a1b8c4160d35210226e5b
한윤석 <hys6078@naver.com>
2021-06-30 13:42:54 +0900

    implement change profile pic

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,6 +4,8 @@ import {UserContext} from '../../App'
 const Profile = ()=>{
     const [mypics, setPics] = useState([])
     const {state,dispatch} = useContext(UserContext)
+    const [image, setImage]=useState("")
+    const [url, setUrl]=useState("")
 
     useEffect(()=>{
         fetch('/mypost',{
@@ -16,13 +18,41 @@ const Profile = ()=>{
         })
      },[])
 
+     useEffect(()=>{
+        if(image){ 
+            const data = new FormData()
+            data.append("file",image)
+            data.append("upload_preset","insta-clone")
+            data.append("cloud_name","dpfd7tict")
+            fetch("https://api.cloudinary.com/v1_1/hyoonseok/image/upload",{
+            method:"post",
+            body:data
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                setUrl(data.url) //useEffect 호출
+                localStorage.setItem("user",JSON.stringify({...state,pic:data.url}))
+                dispatch({type:"UPDATEPIC",payload:data.url})
+            })
+            .catch(err=>{ 
+                console.log(err)
+            })
+        }
+     },[image]) //image를 넘기는 거
+
+    const updatePhoto=(file)=>{
+        setImage(file)
+    }
+
     return(
         <div style={{maxWidth:"550px", margin:"0px auto"}}>
             <div style={{
-                display:"flex",
-                justifyContent:"space-around",
                 margin:"18px 0px",
                 borderBottom:"1px solid grey"
+            }}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
             }}>
                 <div>
                     <img alt="" style={{width:"160px",height:"160px", borderRadius:"80px"}}
@@ -33,8 +63,18 @@ const Profile = ()=>{
                     <h5>{state?state.email:"loading"}</h5>
                     <div style={{display:"flex",justifyContent:"space-between", width:"108%"}}>
                         <h6>{mypics.length} posts</h6>
-                        <h6>{state?state.followers.length:"0"} followers</h6>
-                        <h6>{state?state.following.length:"0"} following</h6>
+                        <h6>{state.followers != undefined?state.followers.length:"0"} followers</h6>
+                        <h6>{state.following != undefined?state.following.length:"0"} following</h6>
+                    </div>
+                </div>
+            </div>
+            <div className="file-field input-field" style={{margin:"10px"}}>
+                    <div className="btn #64b5f6 blue darken-1">
+                        <span>Update pic</span>
+                        <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])}/>
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text"/>
                     </div>
                 </div>
             </div>
